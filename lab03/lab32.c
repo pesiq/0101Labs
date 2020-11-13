658604e710c287bac393fa1b332d12b0891fa8c5
b2050414 <who@who.who>
2020-11-13 11:29:07 +0300

    lab3 v0.0.2

--- lab03/lab32.c
+++ lab03/lab32.c
@@ -5,7 +5,9 @@
 
 #include <stdio.h>
 #include <string.h>
-#define MLEN 200
+#include <time.h>
+#include <stdlib.h>
+#define MLEN 500
 
 void strwrk (char in[], char out[]){
 	int i,k,l,j;
@@ -21,12 +23,11 @@ void strwrk (char in[], char out[]){
 			wl[j]= k;
 			j++;
 		}
-
-		for(i = 0; i < j; ++i){
-			for(l = 0; l < i; ++l){
+	}
+		for(i = 0; i < j-1; i++){
+			for(l = 0; l < j-i-1; l++){
 				k = wl[l];
 				if(wl[l] > wl[l+1]) k = wl[l+1];
-				printf("%d %d",i,l);
 				if(strncmp(w[l],w[l+1], k)>0){
 				
 				char* tc = w[l];
@@ -39,23 +40,67 @@ void strwrk (char in[], char out[]){
 				}
 			}
 		}
-				//if(l!=0) strcat(out," ");
-		//strncat(out, in+i, k);
-	}
+	
 	for(i = 0; i < j; ++i){
 		if(i!=0) strcat(out," ");
 		strncat(out, w[i], wl[i]);
 		}
+
 }
+
+char *getstring(){
+
+char buffer[121] = {0};
+char *res = NULL;
+int len = 0;
+int n = 0;
+
+	do{
+		n = scanf("%120[^\n]", buffer);
+		if(n < 0){
+			if(!res){
+				return NULL;
+			}
+		} else if ( n > 0){
+		int part_len = strlen(buffer);
+		int str_len = len + part_len;
+		res = realloc(res, str_len+1);
+		memcpy(res+len, buffer, part_len);
+		len = str_len;
+		} else {
+			scanf("%*c");
+			}
+
+	}while (n > 0);
+
+	if(len > 0){
+	res[len] = '\0';
+	} else {
+	res = calloc(1, sizeof(char));
+	}
+	return res;
+}
+
 
 int main(){
 
-char words[] = {"		aa  aab 	asdfl ksldkfq 	poas poar nae werl asdop	 asme pikw cxcv asdiw dfvn		 x lqweo zxcsd pojret  	 	sdf olaira vsdip newrk aslc	 		"};
+char *words = NULL;
+do{
+words = getstring();
+if(words){
+	pritnf("%s\n", words);
+	free(words);
+	}
+}while(words);
+
 char out[MLEN]; 
 
-strwrk(words, out);
+printf("%s", words);
 
-printf("\"%s\" \n", out); 
+strwrk(words, out);
+double time = clock();
+printf("\"%s\" \n", out);
+printf("Total time: %.81f seconds\n", time/CLOCKS_PER_SEC);
 
 return 0;
 
