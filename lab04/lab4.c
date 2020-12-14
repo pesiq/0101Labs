b8ddeedf8ddef2fa9153e8e35c7aab7b419e6f5d
Ruslan <russlan2002@inbox.ru>
2020-12-14 22:36:07 +0300

    kb input probably works

--- lab04/lab4.c
+++ lab04/lab4.c
@@ -4,73 +4,67 @@
 #include <time.h>
 
 
-void strwrk(char *input);
+void strwrk(char *input, int type);
+
 char *getstring();
-void insertion_sort(char **words, int n);
-void bubble_sort(char **words, int n);
+
+void insertion_sort(char **words, int *word_lengths, int amount);
+
+void bubble_sort(char **words, int *word_lengths, int amount);
+
 char *letter_freq(char input[]);
+
 void keyboard_input();
-char **tokenize(char *in, int amount);
-char *detokenize(char **in, int size, int n);
+
 void file_input();
-//int generate_random();
 
-void strwrk(char *input){
-	char ** w, * p;
-	int * wl, l, i;
-	w=malloc(strlen(input)/2*sizeof(char *));
-	wl=malloc(strlen(input)/2*sizeof(int));
-	for (i=0, p=input; *(p+=strspn(p, " \t")); p+=wl[i++]){
-		w[i]=p;
-		wl[i]=strcspn(p, " \t");
-	}
-	l=i;
-	w=realloc(w, l*sizeof(char *));
-	wl=realloc(wl, l*sizeof(int));
-	for (i=0; i<l; i++)
-		printf("'%.*s' - %d\n", wl[i], w[i], wl[i]);
-	
+void random_input();
 
-	free(w);
-	free(wl);
-/*    int input_len = strlen(input);
-    //Вычисление количества слов (кол-во пробелов + 1)
-    int word_amount = 1, i;
-    for(i = 0; input[i]; i++){
-        word_amount += (input[i] == ' ');
+char *generate_random();
+
+void strwrk(char *input, int type) {
+    char **words, *p;
+    int *wl, l, i, len = strlen(input), ln = 0;
+    words = malloc(len / 2 * sizeof(char *));
+    wl = malloc(len / 2 * sizeof(int));
+    for (i = 0, p = input; *(p += strspn(p, " \t")); p += wl[i++]) {
+        words[i] = p;
+        wl[i] = strcspn(p, " \t");
+    }
+    l = i;
+    words = realloc(words, l * sizeof(char *));
+    wl = realloc(wl, l * sizeof(int));
+
+    double time;
+    clock_t t;
+    switch (type) {
+        case 1:
+            t = clock();
+            bubble_sort(words, wl, l);
+            time = (double) (clock() - t) / CLOCKS_PER_SEC;
+            break;
+        case 2:
+            t = clock();
+            insertion_sort(words, wl, l);
+            time = (double) (clock() - t) / CLOCKS_PER_SEC;
+            break;
     }
 
-    //Предобработка строк для сортировки
-    char *bubble_str = calloc(strlen(input)+1, sizeof(char));
-    memcpy(bubble_str, input, input_len+1);
-    char *insertion_str = calloc(strlen(input)+1, sizeof(char));
-    memcpy(insertion_str, input, input_len+1);
+    char *output = calloc(len, sizeof(char));
+    char *freq = letter_freq(input);
+    for (i = 0; i < l; i++) {
+        memcpy(output, words[i], wl[i]);
+        ln += wl[i];
+    }
+    output[ln] = '\0';
+    printf("Input string was '%s'\nSorted string is '%s'\nTime was: %.30f\nLetter frequency: %s\n", input, output, time,
+           freq);
 
-    //Токенизация строки для сортировки пузырьком
-    char **bubble_words = tokenize(bubble_str, word_amount);
-
-    clock_t sort_time = clock(); //Начало таймера сортировки
-    bubble_sort(bubble_words, word_amount);
-    sort_time = clock() - sort_time;
-    double bubble_time = (double) (sort_time / CLOCKS_PER_SEC);//Конец таймера сортировки
-
-    //Собирается строка из отсортированных пузырьком слов
-    char *bubble_out = detokenize(bubble_words, input_len, word_amount);
-    free(bubble_str); //Освобождается строка для сортировки пузырьком
-
-    //Токенизация строки для сортировки вставками
-    char **insertion_words = tokenize(insertion_str, word_amount);
-
-    sort_time = clock();     //Начало таймера сортировки
-    insertion_sort(insertion_words, word_amount);
-    double insertion_time = ((clock() - sort_time)/CLOCKS_PER_SEC); //Конец таймера сортировки
-
-    //Собирается результат сортировки вставками
-    char *insertion_out = detokenize(insertion_words, input_len, word_amount);
-    free(insertion_str); //Освобождается строка для сортировки вставками
-
-    char *freq = letter_freq(input); // Находится количество кажого символа в строке
-
+    free(words);
+    free(wl);
+    free(output);
+    free(freq);
+/*
     //Вывод результатов
     printf("Input string was:\n\"%s\"\nResult of bubble sort is:\n\"%s\"\nWith time of: %.50f\nResult of insertion sort is:\n\"%s\"\nWith time of: %.50f\n", input, bubble_out, bubble_time, insertion_out, insertion_time);
     printf("Frequency of each character used\n");
@@ -99,8 +93,8 @@ void strwrk(char *input){
 }
 
 
-void bubble_sort(char **words, int amount) {
-    int i, j;
+void bubble_sort(char **words, int *word_lengths, int amount) {
+    int i, j, t;
     char *temp;
     for (i = 0; i < amount - 1; i++) {
         for (j = amount - 1; j > i; j--) {
@@ -108,44 +102,51 @@ void bubble_sort(char **words, int amount) {
                 temp = words[j - 1];
                 words[j - 1] = words[j];
                 words[j] = temp;
+
+                t = word_lengths[j - 1];
+                word_lengths[j - 1] = word_lengths[j];
+                word_lengths[j] = t;
             }
         }
     }
 }
 
-void insertion_sort(char **words, int n){
-    int i, j;
+void insertion_sort(char **words, int *word_lengths, int amount) {
+    int i, j, t;
     char *temp;
-    for(i = 0; i < n-1; i++){
-        for(j = i+1; j < n; j++){
-            if(strcmp(words[i], words[j]) > 0){
+    for (i = 0; i < amount - 1; i++) {
+        for (j = i + 1; j < amount; j++) {
+            if (strcmp(words[i], words[j]) > 0) {
                 temp = words[j];
                 words[j] = words[i];
                 words[i] = temp;
 
+                t = word_lengths[j];
+                word_lengths[j] = word_lengths[i];
+                word_lengths[i] = t;
             }
         }
     }
 }
 
-char *letter_freq(char *input){
+char *letter_freq(char *input) {
     char *out = NULL, *workingstr = NULL, temp[120];
     int i, j, len = strlen(input), counter = 0, out_len = 0;
     workingstr = calloc(strlen(input), sizeof(char));
     memcpy(workingstr, input, len);
-    for(i =0; i < len; i++){
+    for (i = 0; i < len; i++) {
         counter = 1;
-        if(workingstr[i]) {
-            for(j = i + 1; j < len; j++){
-                if(workingstr[i] == workingstr[j]){
+        if (workingstr[i]) {
+            for (j = i + 1; j < len; j++) {
+                if (workingstr[i] == workingstr[j]) {
                     counter++;
                     workingstr[j] = '\0';
                 }
             }
-            sprintf(temp, "'%c' = %d\n", input[i], counter);
+            sprintf(temp, "'%c' = %d", input[i], counter);
             int part_len = strlen(temp);
-            out = realloc(out, part_len+out_len+1);
-            memcpy(out+out_len, temp, part_len);
+            out = realloc(out, part_len + out_len + 1);
+            memcpy(out + out_len, temp, part_len);
             out_len = out_len + part_len;
         }
     }
@@ -153,38 +154,20 @@ char *letter_freq(char *input){
     free(workingstr);
     return out;
 }
-//Во входной строке пробелы заменяются на \0 функция выводит массив указателей на начала слов
-char **tokenize(char *in, int amount){
-    int new_word, i, j;
-    char **out = calloc(amount, sizeof(char *));
-    for(j = 0, i = 0; in[i]; i++){
-        if(in[i] == ' '){
-            in[i] = '\0';
-            new_word = 1;
-        }
-        else if(new_word){
-            out[j++] = &in[i];
-            new_word = 0;
-        }
-    }
-    return out;
-}
 
-char *detokenize(char **in, int size, int n){
-    char *out = calloc(size, sizeof(char));
-    int i, len = 0;
-    for(i = 0; i < n; i++){
-        int part_len = strlen(in[i]);
-        if(i != 0) {
-            memcpy(out+len++, " ", 1);
-        }
-        memcpy(out + len, in[i], part_len);
-        len += part_len;
-    }
-    out[len] = '\0';
-    return out;
-}
+char *generate_random() {
 
+    const char charset[] = "    abcdef ghijklmnopqrstuv wxyz ";
+    int len = rand() % 50 + 20;
+    char *dest = calloc(len + 1, sizeof(char));
+
+    int i;
+    for (i = 0; i < len; i++) {
+        dest[i] = charset[rand() % strlen(charset)];
+    }
+    dest[len] = '\0';
+    return dest;
+}
 
 char *getstring() {
 
@@ -219,39 +202,65 @@ char *getstring() {
     return res;
 }
 
-void keyboard_input(){
-    char *input;
-    input = getstring();
-    strwrk(input);
-    free(input);
+void keyboard_input() {
+
+    int amount, type, i;
+    char **strings;
+
+    printf("Enter an amount of strings\n");
+    while (scanf("%d", &amount) != 1) {
+        scanf("%*[^\n]");
+    }
+    strings = malloc(amount * sizeof(char *));
+    scanf("%*c");
+    printf("Enter %d strings to work with\n", amount);
+    for (i = 0; i < amount; i++) {
+        strings[i] = getstring();
+    }
+    printf("Choose which sorting algorithm to use. Type 1 or 2\n 1. Bubble sort\n 2. Insertion sort\n");
+    do {
+        if (scanf("%d", &type) != 1) {
+            scanf("%*[^\n]");
+        }
+    } while (type != 1 && type != 2);
+    scanf("%*c");
+    for (i = 0; i < amount; i++) {
+        strwrk(strings[i], type);
+        free(strings[i]);
+    }
+    free(strings);
 }
 
-void file_input(){
-    char input[10000];
-    char name[100];
+void file_input() {
+    char input[10000], name[100];
+    int type = 1;
     FILE *file;
     scanf("%s", name);
-    while(!(file = fopen(name, "r"))){
+    while (!(file = fopen(name, "r"))) {
         printf("Cannot open file!\n");
         scanf("%s", name);
     }
     fgets(input, 10000, file);
-    strwrk(input);
+    strwrk(input, type);
 }
 
-int main(){
-	printf("Starting...\n");
+void random_input() {
+
+}
+
+int main() {
+    printf("Starting...\n");
     int status = 1, menu;
-    while (status){
+    while (status) {
         printf("Please enter a number 1 - 4\n1: Input string from keyboard\n2: Input from file\n3: Generate random string\n4: Quit\n");
-        if(scanf("%d", &menu) != 1) {
-			scanf("%*[^\n]");
+        if (scanf("%d", &menu) != 1) {
+            scanf("%*[^\n]");
             menu = 0;
         }
-		scanf("%*c");
+        scanf("%*c");
         switch (menu) {
             case 1:
-                printf("-----Option one - keyboard input-----\nEnter a string to work with\n");
+                printf("-----Option one - keyboard input-----\n");
                 keyboard_input();
                 break;
             case 2:
@@ -260,7 +269,7 @@ int main(){
                 break;
             case 3:
                 printf("-----Option three - random input-----\nEnter seed to generate a random string\n");
-                //generate_random();
+                generate_random();
                 break;
             case 4:
                 status = 0;
@@ -270,6 +279,6 @@ int main(){
                 break;
         }
     }
-	printf("Exiting...\n");
-	return 0;
+    printf("Exiting...\n");
+    return 0;
 }
